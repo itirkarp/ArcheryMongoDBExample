c30827d3b32b06ec3b771bbce2f56dab1693294f
Prakriti Mateti <itirkarp@gmail.com>
2013-11-16 23:03:42 +1100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ArcheryMongodbExample::Application.config.secret_key_base = '9bd94094e94c3b1ce9eaac2b8ceaa7f094cdc45e40c5469268c43be8aa07da54f69cfebe26dfcc5b8004112ffd03972ab5fb79d16fcc02e06d9f37d5ee8a0b54'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ArcheryMongodbExample::Application.config.session_store :cookie_store, key: '_archery_mongodb_example_session'
